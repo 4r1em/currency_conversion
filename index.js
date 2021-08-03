6f54b33ac7806ea1106d1fceac3678a1f828f419
4r1em <art.beloshapka@gmail.com>
2021-08-03 13:43:16 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -2,36 +2,41 @@ const request = require('request');
 const url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 const express = require('express');
 const app = new express()
+const currse = ["UAH", "RUR", "USD", "EUR"]
 
 app.use(express.json());
 
 
-// Возможные валюты USD RUR EUR
-
 app.post('/curse', async (req, res) => {
+
+    const sell = req.body[0].sell
+    const buy = req.body[0].buy
+    const mon = req.body[0].mon
+
+    if (!+mon || !mon) res.send("Введите сумму")
+    if (!currse.includes(sell) || !currse.includes(buy) || sell.length == 0 || buy.length == 0) {
+        res.send(`Введите валюту с этого списка ${currse}`)
+    }
+
     await request({ url: url, json: true }, async (err, { body }) => {
-        if (req.body[0].sell.length == 0 || req.body[0].buy.length == 0) res.send("Need valuta")
-        else if (req.body[0].sell || req.body[0].sell == 'BTC') res.send('УСТАЛ ДУМАТЬ')
-        else if (req.body[0].sell == 'UAH') {
+        if (sell == 'UAH') {
             let cashBuy = body.find(item => {
-                if (item.ccy == req.body[0].buy) {
+                if (item.ccy == buy) {
                     return item
                 }
             })
-            if (!cashBuy) res.send(`Нет такой валюты ${req.body[0].buy}`)
             if (cashBuy) {
-                let exchange = req.body[0].mon / cashBuy['buy']
+                let exchange = mon / cashBuy['buy']
                 res.send(`Buy = ${exchange} ${cashBuy['ccy']}`)
             }
-        } else if (req.body[0].sell == 'USD' || 'EUR' || 'RUR') {
+        } else if (sell == 'USD' || sell == 'EUR' || sell == 'RUR') {
             let cashSell = body.find(item => {
-                if (item.ccy == req.body[0].sell) {
+                if (item.ccy == sell) {
                     return item
                 }
             })
-            if (!cashSell) res.send(`Нет такой валюты ${req.body[0].sell}`)
             if (cashSell) {
-                let exchange = req.body[0].mon * cashSell['buy']
+                let exchange = mon * cashSell['buy']
                 res.send(`Sell = ${exchange} UAH`)
             }
         }
